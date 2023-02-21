2cdd972694c1209a7cf69a2e6b9775f8de4c4441
ash23090 <ashwinkumar2309@protonmail.com>
2023-02-21 19:33:15 +0530

    final code
    video calls

---
+++ client/build/precache-manifest.38a2036209ea5f994d38c78acd9a8ba3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83757abb4eb609a3c53055cb6327fb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f22e0222aa92399f807",
+    "url": "/static/css/main.e081a844.chunk.css"
+  },
+  {
+    "revision": "6e1a02e9411fe4ff52f2",
+    "url": "/static/js/2.a8460e7d.chunk.js"
+  },
+  {
+    "revision": "78b287351bb08f453fa0918ea10ac39c",
+    "url": "/static/js/2.a8460e7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f22e0222aa92399f807",
+    "url": "/static/js/main.578fe61c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
